3faff4ea8151e503887d9fbb830ac5330708d9df
Oleg Sviridov <olegsv3007@yandex.ru>
2025-03-23 23:30:01 +0200

    Implementation of previous step

--- symfony/src/Application/Query/Handler/GetAllTasksQueryHandler.php
+++ symfony/src/Application/Query/Handler/GetAllTasksQueryHandler.php
@@ -2,7 +2,24 @@
 
 namespace App\Application\Query\Handler;
 
+use App\Application\Query\GetAllTasksQuery;
+use App\Domain\Entity\Task;
+use App\Domain\Repository\TaskRepositoryInterface;
+
 class GetAllTasksQueryHandler
 {
+    private TaskRepositoryInterface $taskRepository;
 
+    public function __construct(TaskRepositoryInterface $taskRepository)
+    {
+        $this->taskRepository = $taskRepository;
+    }
+
+    /**
+     * @return array<int, Task>
+     */
+    public function __invoke(GetAllTasksQuery $query): array
+    {
+        return $this->taskRepository->findAll();
+    }
 }
