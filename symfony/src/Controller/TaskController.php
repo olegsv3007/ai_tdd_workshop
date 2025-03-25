0a5998804864827baa05d728972b2a411a1fefbf
Oleg Sviridov <olegsv3007@yandex.ru>
2025-03-25 12:08:12 +0200

    Implement Create task action

--- symfony/src/Controller/TaskController.php
+++ symfony/src/Controller/TaskController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Application\Bus\CommandBusContract;
 use App\Application\Bus\QueryBusContract;
+use App\Application\Command\CreateTaskCommand;
 use App\Application\Query\GetAllTasksQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
@@ -22,5 +25,16 @@ class TaskController extends AbstractController
         $tasks = $this->queryBus->query(new GetAllTasksQuery());
 
         return $this->json($tasks, Response::HTTP_OK, [], ['groups' => 'task:read']);
+    }
+
+    #[Route('/api/tasks', name: 'task_create', methods: ['POST'])]
+    public function createTask(
+        #[MapRequestPayload] CreateTaskCommand $command,
+        CommandBusContract $commandBus,
+    ): JsonResponse
+    {
+        $commandBus->dispatch($command);
+
+        return new JsonResponse([], Response::HTTP_CREATED);
     }
 }
